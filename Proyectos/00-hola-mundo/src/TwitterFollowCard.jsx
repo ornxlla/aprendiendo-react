f81f7e80e89b1b6577a37813b03a96c89613dc7f
Ornella Alonso Reyes <ornxalonso@gmail.com>
2024-12-01 16:10:24 -0300

    Pequeños cambios en estilos

--- Proyectos/00-hola-mundo/src/TwitterFollowCard.jsx
+++ Proyectos/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -26,7 +26,8 @@ export function TwitterFollowCard({children, userName, initialIsFollowing }){
         </header>
         <aside>
             <button className={buttonClassName} onClick={handleClick}>
-                {text}
+                <span className='tw-followCard-text'>{text}</span>
+                <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
             </button>
         </aside>
         </article>
